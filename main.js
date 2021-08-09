4b24f5f87e38121e677165378ad9509f31c852a5
hoonion90 <clearch21@gmail.com>
2021-08-10 01:04:44 +0900

    change package.json setting

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
- 
+  var url = request.url;
+  if(request.url == '/'){
+    url = '/index.html';
+  }
+  if(request.url == '/favicon.ico'){
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  response.end(fs.readFileSync(__dirname + url));
 });
-app.listen(3000);
+const PORT = process.env.PORT
+// app.listen(3000);
+app.listen(PORT);
