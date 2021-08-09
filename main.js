29dff43add2969d3162770fcc8ebf3c26aeb79e3
hoonion90 <clearch21@gmail.com>
2021-08-10 00:49:05 +0900

    change port

--- main.js
+++ main.js
@@ -12,6 +12,4 @@ var app = http.createServer(function(request,response){
     response.end(fs.readFileSync(__dirname + url));
  
 });
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(3000);
